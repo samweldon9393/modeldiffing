dbc9c32fc62d358232ef39d8863694726daf1d12
Sam <samweldon9393@gmail.com>
2025-06-11 12:26:41 -0400

    Implement regex pattern, seems to work

--- src/models/answer_checker.py
+++ src/models/answer_checker.py
@@ -1,10 +1,16 @@
-class answer_checker:
-    def __init__():
+import re
+
+class AnswerChecker:
+    def __init__(self):
         pass
 
     #single prediction & ground truth for now 
-    def check(prediction, ground_truth):
+    def check(self, prediction, ground_truth):
         reward = 0 
 
-    def format(prediction):
-        #look for 
+    def format(self, prediction):
+        #match[0] = whole string, match[2] = inside think tags, match[4] = final answer
+        match = re.search("(.*)<think>(.*?)</think>(.*?)\\\\boxed{(.*)}", prediction)
+
+ac = AnswerChecker()
+ac.format('<think>1 plus 1 might equal 2</think>\\boxed{2}')
