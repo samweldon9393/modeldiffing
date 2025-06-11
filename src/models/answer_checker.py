88b12e284f4eae367b346f1678ca9c88ed3829cb
Sam <samweldon9393@gmail.com>
2025-06-11 12:10:54 -0400

    first commit, add file for testing answer checker

---
+++ src/models/answer_checker.py
@@ -0,0 +1,10 @@
+class answer_checker:
+    def __init__():
+        pass
+
+    #single prediction & ground truth for now 
+    def check(prediction, ground_truth):
+        reward = 0 
+
+    def format(prediction):
+        #look for 
