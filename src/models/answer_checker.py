a1f93e1e5f755b6340e4452eea700e7732ce45cc
Sam <samweldon9393@gmail.com>
2025-06-11 13:37:21 -0400

    add batched check method

--- src/models/answer_checker.py
+++ src/models/answer_checker.py
@@ -1,11 +1,22 @@
 import re
+import json
 
 class AnswerChecker:
     def __init__(self):
         pass
 
+    def check_batched(self, predictions: list[str], ground_truths: list[str]) -> list[dict]:
+        if len(predictions) != len(ground_truths):
+            raise Exception("Must have same quantity of predictions and ground truths")
+
+        rewards = []
+        for i, prediction in enumerate(predictions):
+            rewards.append( self.check(prediction, ground_truths[i]) )
+
+        return rewards
+
     #single prediction & ground truth for now 
-    def check(self, prediction, ground_truth):
+    def check(self, prediction: str, ground_truth: str) -> dict:
         info = {
             "format_correct" : 0,
             "answer_correct" : 0,
@@ -41,27 +52,26 @@ class AnswerChecker:
         return info
 
 
-    def response_format(self, prediction):
+    def response_format(self, prediction: str):
         #match[0] = whole string, match[2] = inside think tags, match[4] = final answer
         match = re.search("(.*)<think>(.*?)</think>(.*?)\\\\boxed{(.*)}", prediction)
         return match
     
-    def truth_format(self, ground_truth):
+    def truth_format(self, ground_truth: str):
         #match[1] = final answer
         match = re.search("###\\s*(\\d+)", ground_truth)
         return match
 
-    def first_number(self, prediction):
+    def first_number(self, prediction: str):
         #match[0] = first number in string
         match = re.search(r"\d+", prediction)
         return match
 
 ac = AnswerChecker()
 
-#response = "<think>1 plus 1 might equal 2</think> \\boxed{2} adasd"
-response = "hello 2 friend  sasdfaf"
-ground_truth = " ... ### 2"
+responses = ["hello 2 friend  sasdfaf", "<think>1 plus 1 might equal 2</think> \\boxed{2} adasd"]
+ground_truths = [" ... ### 2", "this is the answer ### 2"]
 
-info = ac.check(response, ground_truth)
+info = ac.check_batched(responses, ground_truths)
 
 print(info)
