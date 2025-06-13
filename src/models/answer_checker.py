ae6fbf7c74e2c7bf5d6451d27a4fc0baa10c685c
Sam <samweldon9393@gmail.com>
2025-06-13 14:02:20 -0400

    fix regex

--- src/models/answer_checker.py
+++ src/models/answer_checker.py
@@ -1,75 +1,59 @@
 import regex as re
 import json
-#from concurrent.futures import ThreadPoolExecutor
-#from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Pool
-from more_itertools import chunked
 
 import cProfile
 import pstats
 
-
-class AnswerChecker:
-    def __init__(self):
-        #match[0] = whole string, match[2] = inside think tags, match[4] = final answer
-        self.response_pattern = re.compile("(.*)<think>(.*?)</think>(.*?)\\\\boxed{(.*)}")
-        #match[1] = final answer
-        self.truth_pattern = re.compile("###\\s*(\\d+)")
-        #match[0] = first number in string
-        # TODO this will pull a number from inside the think tags, is that what we want?
-        # this one is easy w/o regex but it should rarely need to be used
-        self.first_number_pattern = re.compile("\\d+")
+from multiprocessing import Pool
 
 
+#match[0] = whole string, match[2] = final answer
+#response_pattern = re.compile(r"<think>(.*?)</think>\s*\\boxed{(.*)}")
+response_pattern = re.compile(r"<think>(.*?)</think>\s*\\\\boxed{(.*)}")
+#match[1] = final answer
+truth_pattern = re.compile("###\\s*(\\d+)")
+#match[0] = first number in string
+# TODO this will pull a number from inside the think tags, is that what we want?
+first_number_pattern = re.compile("\\d+")
 
-    def check_batched(self, predictions: list[str], ground_truths: list[str]) -> list[dict]:
-        if len(predictions) != len(ground_truths):
-            raise Exception("Must have same quantity of predictions and ground truths")
+def check_batch(batch):
+    return [check(pred, gt) for pred, gt in batch]
 
-        rewards = []
-        #args = list(zip(predictions, ground_truths))
-        batches = list(chunked(zip(predictions, ground_truths), 1000))
+def check_batched(predictions: list[str], ground_truths: list[str]) -> list[dict]:
+    if len(predictions) != len(ground_truths):
+        raise Exception("Must have same quantity of predictions and ground truths")
 
-        '''
-        #sequential
-        for i, prediction in enumerate(predictions):
-            rewards.append( self.check(prediction, ground_truths[i]) )
-        '''
-        '''
-        # threads
-        with ThreadPoolExecutor() as executor:
-            rewards = list(executor.map(check, args))
-        '''
-        # processes
-        with ProcessPoolExecutor() as executor:
-            rewards = list(executor.map(check, args))
+    #sequential
+    rewards = [check(p, gt) for p, gt in zip(predictions, ground_truths)]
 
-        with open("rewards_log.json", "w") as f:
-            json.dump(rewards, f, indent=4)
+    with open("rewards_log.json", "w") as f:
+        json.dump(rewards, f, indent=4)
 
-        return rewards
+    return rewards
 
 #single prediction & ground truth 
-def check(ac: AnswerChecker, prediction: str, ground_truth: str) -> dict:
+def check(prediction: str, ground_truth: str) -> dict:
     info = {
         "format_correct" : 0,
         "answer_correct" : 0,
         "reward" : 0
     }
 
-    response_match = ac.response_pattern.search(prediction)
+    print(repr(prediction))
+    response_match = response_pattern.search(prediction)
     ans = "x" # cannot match truth
     if response_match != None:
         info["format_correct"] = 1
-        ans = response_match[4] 
+        ans = response_match[2] 
     else:
-        match = ac.first_number_pattern.search(prediction)
+        match = first_number_pattern.search(prediction)
         if match == None:
             # TODO log parse fail
             return info # 0 reward for no match
         ans = match[0]
+        # TODO first number probably gets the wrong answer more than last number would
 
-    truth_match = ac.truth_pattern.search(ground_truth)
+    truth_match = truth_pattern.search(ground_truth)
     if truth_match == None:
         # TODO log parse fail
         return info # ground truth parse failure
@@ -79,6 +63,7 @@ def check(ac: AnswerChecker, prediction: str, ground_truth: str) -> dict:
             info["answer_correct"] = 1
     except(ValueError, TypeError):
         # TODO log parse fail
+
         return info # integer parse error
         # ask Paul what we want to do here
 
@@ -88,16 +73,14 @@ def check(ac: AnswerChecker, prediction: str, ground_truth: str) -> dict:
 
 def test():
 
-    with open("responses.txt", "r") as f:
+    with open("short_responses.txt", "r") as f:
         content = f.read()
     predictions = content.strip().split("\n\n")
-    with open("ground_truths.txt", "r") as f:
+    with open("short_truths.txt", "r") as f:
         content = f.read()
     truths = content.strip().split("\n\n")
 
-    ac = AnswerChecker()
-    rewards = ac.check_batched(predictions, truths)
-    #rewards = [check(ac, p, gt) for p, gt in zip(predictions, truths)]
+    rewards = check_batched(predictions, truths)
 
 profiler = cProfile.Profile()
 profiler.enable()
